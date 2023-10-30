c7764fce95b65f78172ba9a5247beed398a8ad68
Dzmitry <ruban251219872023@gmail.com>
2023-10-30 13:12:42 +0300

    Add files via upload

---
+++ training.js
@@ -0,0 +1,14 @@
+
+
+var person = null;
+if(confirm('Приветствуем Вас на сайте!')) {
+    person = prompt('Введите ваше имя');
+    alert('Привет:' + person);
+    
+} else {
+    alert('Мы вас ждем снова!')
+
+}
+
+
+
